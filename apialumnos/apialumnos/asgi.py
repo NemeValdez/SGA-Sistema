cbb7ee2b30cdfd3f0c3174aafd0d5d3fe47e8879
NemeValdez <damiancarnevali@hotmail.com>
2021-12-07 04:59:52 -0300

    viendo

--- apialumnos/apialumnos/asgi.py
+++ apialumnos/apialumnos/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apialumnos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apialumnos.settings.py')
 
 application = get_asgi_application()
