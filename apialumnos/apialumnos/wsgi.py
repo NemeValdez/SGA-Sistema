f8afa2dd975f13ea8e37a6a010dc0404034f1c69
NemeValdez <damiancarnevali@hotmail.com>
2021-12-07 04:55:32 -0300

    problema con el settings

--- apialumnos/apialumnos/wsgi.py
+++ apialumnos/apialumnos/wsgi.py
@@ -1,7 +1,7 @@
 import os
-
+from apialumnos import settings
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apialumnos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apialumnos.settings.py')
 
 application = get_wsgi_application()
