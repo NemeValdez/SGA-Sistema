2815a88aae78bc12bd0dfe622137b986182efb12
NemeValdez <damiancarnevali@hotmail.com>
2021-12-08 01:34:29 -0300

    con urls

--- apialumnos/app/estudiantes/urls.py
+++ apialumnos/app/estudiantes/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from app.estudiantes.views import EstudiantesEstablecimientosViewSet, EstudiantesViewSet
+from app.estudiantes.views import EstudiantesEstablecimientosViewSet, EstudiantesViewSet, BuscarDNIView
 
 routers = DefaultRouter()
 routers.register(r'escuela', EstudiantesEstablecimientosViewSet,
                  basename='Secundario de donde proviene el estudiante')
 routers.register(r'estudiante', EstudiantesViewSet,
                  basename='Datos del estudiante')
+routers.register(r'buscar-dni', BuscarDNIView, basename='dni')
 urlpatterns = routers.urls
