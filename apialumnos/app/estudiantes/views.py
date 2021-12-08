abb972e87ffb025ad3f62afa75b2fa598cc19c77
NemeValdez <damiancarnevali@hotmail.com>
2021-12-07 23:25:31 -0300

    filtros

--- apialumnos/app/estudiantes/views.py
+++ apialumnos/app/estudiantes/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, generics
+from django_filters.rest_framework import DjangoFilterBackend
 from app.estudiantes.serializers import EstudiantesSerializers, EstudiantesEstablecimientosSerializers
 
 
@@ -14,9 +15,9 @@ class EstudiantesViewSet(viewsets.ModelViewSet):
     queryset = EstudiantesSerializers.Meta.model.objects.all()
 
 
-class BuscarDNIView(viewsets.ReadOnlyModelViewSet):
+class BuscarDNIView(generics.ListAPIView):
     '''Vista que permite buscar por DNI a los registrados como estudiantes'''
-    serializer_class = EstudiantesSerializers
     queryset = EstudiantesSerializers.Meta.model.objects.all()
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['dni_estudiante']
+    serializer_class = EstudiantesSerializers
+    filter_backends = (DjangoFilterBackend)
+    filterset_fields = ('dni_estudiante')
