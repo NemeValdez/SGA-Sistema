4841c31cd666fac6f18441e4aa8008e2a7f143fa
NemeValdez <damiancarnevali@hotmail.com>
2021-12-08 00:21:45 -0300

    retrieve dni

--- apialumnos/app/estudiantes/views.py
+++ apialumnos/app/estudiantes/views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets, generics
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from app.estudiantes.models import Estudiantes
 from app.estudiantes.serializers import EstudiantesSerializers, EstudiantesEstablecimientosSerializers
 
@@ -16,9 +16,21 @@ class EstudiantesViewSet(viewsets.ModelViewSet):
     queryset = EstudiantesSerializers.Meta.model.objects.all()
 
 
-class BuscarDNIView(generics.ListAPIView):
+class BuscarDNIView(viewsets.ModelViewSet):
     '''Vista que permite buscar por DNI a los registrados como estudiantes'''
-    queryset = Estudiantes.objects.all()
-    serializer_class = EstudiantesSerializers
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['dni_estudiante']
+    serializer_class = EstudiantesSerializers()
+
+    def get_queryset(self, pk=None):
+        if pk is None:
+            return Response({
+                'mensaje': 'Error en los parámetros de la búsqueda'
+            }, status=status.HTTP_404_NOT_FOUND)
+        return self.get_serializer().Meta.model.objects.filter(dni_estudiante=pk).first()
+
+    def retrieve(self, request, pk=None):
+        buscar_dni = self.get_queryset().filter(dni_estudiante=pk).exists()
+        if buscar_dni:
+            dni = self.get_queryset().filter(dni_estudiante=pk).first()
+            dni_serializer = self.serializer_class(dni)
+            return Response(dni_serializer.data, status=status.HTTP_200_OK)
+        return Response({'mensaje': 'No se han encontrado datos con el parámetro indicado'}, status=status.HTTP_404_NOT_FOUND)
