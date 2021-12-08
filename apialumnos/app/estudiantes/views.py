b63f48d109fee25fb832146ee09d6c79e4cb68a2
NemeValdez <damiancarnevali@hotmail.com>
2021-12-07 23:33:19 -0300

    viendo

--- apialumnos/app/estudiantes/views.py
+++ apialumnos/app/estudiantes/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, generics
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 from app.estudiantes.serializers import EstudiantesSerializers, EstudiantesEstablecimientosSerializers
 
 
