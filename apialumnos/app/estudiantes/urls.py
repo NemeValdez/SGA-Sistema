9a6e574ce8ad565d95926c2f1b9109179781ac0d
NemeValdez <damiancarnevali@hotmail.com>
2021-12-07 21:28:08 -0300

    busqueda dni

--- apialumnos/app/estudiantes/urls.py
+++ apialumnos/app/estudiantes/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
-from app.estudiantes.views import EstudiantesEstablecimientosViewSet, EstudiantesViewSet
+from app.estudiantes.views import EstudiantesEstablecimientosViewSet, EstudiantesViewSet, BuscarDNIView
 
 routers = DefaultRouter()
 routers.register(r'escuela', EstudiantesEstablecimientosViewSet,
                  basename='Secundario de donde proviene el estudiante')
 routers.register(r'estudiante', EstudiantesViewSet,
                  basename='Datos del estudiante')
+routers.register(r'buscar-dni', BuscarDNIView,
+                 basename='Busqueda filtrada del DNI')
 urlpatterns = routers.urls
